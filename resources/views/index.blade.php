c664ad1b5e29247ecb5242922d7bbbdcd90a7fa9
Ga <gabri.magni.97@gmail.com>
2022-05-25 18:37:51 +0200

    row-cols

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,12 +2,18 @@
 
 @section('content')
 
-    @foreach ($movies as $key => $movie)
-    
-    <a href="{{ route('show', ['id' => $movie->id]) }}">
-        <h2>{{ $movie->title }}</h2>
-    </a>
 
-    @endforeach
+<div class="container">
+    <div class="row row-cols-4 g-3">
+        @foreach ($movies as $key => $movie)
+            <div class="col">
+                <a href="{{ route('show', ['id' => $movie->id]) }}">
+                    <h2>{{ $movie->title }}</h2>
+                </a>
+            </div>
+        @endforeach
+    </div>
+</div>
+
     
 @endsection
