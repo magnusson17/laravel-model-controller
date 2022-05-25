3a704c08d9c835bdb8b8afffeb6e2eadc2367f59
Ga <gabri.magni.97@gmail.com>
2022-05-25 16:54:14 +0200

    model, controller, layout

---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layout.layout')
+
+@section('content')
+
+    {{-- @foreach ($movies as $key => $movie)
+    
+    <h2>{{ $movie->title }}</h2>
+
+    @endforeach --}}
+    <p>ciao</p>
+    
+@endsection
